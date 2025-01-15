de3b3edc044b3e9c2a8eb00e8b554c6e091616ae
adambuenz-cell <adam_buenz@hotmail.com>
2025-01-15 07:47:31 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
-import styles from './Footer.module.css'
+import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
-    <>
-      
-    </>
-  )
+    <footer className={styles.footer}>
+      <p>© 2025 AAIRC, Inc. All rights reserved.</p>
+    </footer>
+  );
 }
